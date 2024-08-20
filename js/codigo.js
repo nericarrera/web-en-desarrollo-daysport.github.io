872881dd319d7e0beaa064da3654832c26102a3e
nericarrera <nericarrera@yahoo.com.ar>
2024-08-20 16:26:03 -0300

    si

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+const cardWrapper = document.querySelector('.card-wrapper');
+const nextArrow = document.getElementById('nextArrow');
+const prevArrow = document.getElementById('prevArrow');
+
+let scrollAmount = 0;
+const scrollStep = 250;
+
+nextArrow.addEventListener('click', function(event) {
+    event.preventDefault();
+    scrollAmount += scrollStep;
+    cardWrapper.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+prevArrow.addEventListener('click', function(event) {
+    event.preventDefault();
+    scrollAmount -= scrollStep;
+    if (scrollAmount < 0) {
+        scrollAmount = 0;
+    }
+    cardWrapper.style.transform = `translateX(-${scrollAmount}px)`;
+});
