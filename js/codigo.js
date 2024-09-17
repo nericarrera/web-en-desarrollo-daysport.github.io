bb332c2eb8297984660359277d825d02385aee15
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-09-17 16:42:01 -0300

    ok

--- js/codigo.js
+++ js/codigo.js
@@ -218,7 +218,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   const products = [
-    { id: 1, name: "Campera Hombre Liviana", gender: "hombre", category: "campera-liviana", size: "M", price: 3500, image: "img/hombre/campera/campera-liviana1.jpg" },
+    { id: 1, name: "Campera Hombre Liviana", gender: "hombre", category: "campera-liviana", size: "M", price: 3500, image: "img/hombre/joggin/WhatsApp Image 2024-07-06 at 22.31.59 (2).jpeg" },
     { id: 2, name: "Remera Mujer", gender: "mujer", category: "remeras", size: "L", price: 2000, image: "img/mujer/remera/remera1.jpg" },
     { id: 3, name: "Campera Mujer de Abrigo", gender: "mujer", category: "campera-abrigo", size: "S", price: 4500, image: "img/mujer/campera/campera-abrigo2.jpg" },
     // Agrega más productos según tus necesidades...
