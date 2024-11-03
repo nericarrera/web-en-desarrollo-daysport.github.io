950f7ee1c35d38cc4f445ff25dc09821ee517aae
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-11-03 10:42:56 -0300

    ok

--- js/mujer.js
+++ js/mujer.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const mujerProductsGrid = document.querySelector('.mujer-products-grid');
     const filterButtons = document.querySelectorAll('.mujer-filter-button');
     const filterDropdownToggle = document.querySelector('.filter-dropdown-toggle');
-    const filterDropdown = document.querySelector('.filter-dropdown.show');
+    const filterDropdown = document.querySelector('.filter-dropdown');
     const applyFiltersButton = document.getElementById('apply-filters');
 
     const productosMujer = [
