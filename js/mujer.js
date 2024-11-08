408271231962978482d855c9e8c4ca47fb60f39d
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-11-08 10:50:14 -0300

    ok

--- js/mujer.js
+++ js/mujer.js
@@ -161,7 +161,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             toggleButton.addEventListener('click', () => {
                 // Alternar visibilidad
-                content.style.display = content.style.display === 'none' ? 'block' : 'none';
+                content.style.display = content.style.display === 'none' ? 'block' : 'block';
 
                 // Alternar símbolo de desplegar/cerrar
                 const symbol = toggleButton.querySelector('span');
