c9041ab9cc41a510b18ac4810396528205ee70be
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-11-08 20:16:58 -0300

    ok

--- js/mujer.js
+++ js/mujer.js
@@ -14,8 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const sortRadios = document.querySelectorAll('input[name="sort"]');
 
     const productosMujer = [
-        { id: 1, nombre: "Remera Modal Soft", precio: 7500, categoria: "remeras", imagen: "img/mujer/remera-modal-soft-cuelloR/remera-modal-soft-cuelloR 1.jpeg", color: "celeste", talla: "XL", etiqueta: "nuevo" },
-        { id: 2, nombre: "Remera Modal Soft", precio: 7500, categoria: "remeras", imagen: "img/mujer/remera-modal-soft-cuelloR/remera-modal-soft-cuelloR 2.jpeg", color: "Negro", talla: "L", etiqueta: "novedades" },
+        { id: 1, nombre: "Remera Modal Soft",
+            precio: 7500, categoria: "remeras",
+            imagen: "img/mujer/remera-modal-soft-cuelloR/remera-modal-soft-cuelloR 1.jpeg", 
+            color: "celeste", 
+            talla: "XL", 
+            etiqueta: "nuevo" 
+        },
+        { id: 2, nombre: "Remera Modal Soft", 
+            precio: 7500, 
+            categoria: "remeras", 
+            imagen: "img/mujer/remera-modal-soft-cuelloR/remera-modal-soft-cuelloR 2.jpeg", 
+            color: "Negro", 
+            talla: "L", 
+            etiqueta: "novedades" 
+        },
+        
+        { id: 2, nombre: "Remera Modal viscosa", 
+            precio: 7500, 
+            categoria: "remeras", 
+            imagen: "img/mujer/remera-modal-viscosa-cuelloR/remera-modal-viscosa-cuelloR 1.jpeg", 
+            color: "Blanco", 
+            talla: "L", 
+            etiqueta: "novedades" 
+        },
         // Agrega más productos según sea necesario
     ];
 
