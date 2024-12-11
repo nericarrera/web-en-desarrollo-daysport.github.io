1ac87febcc250596303bb88b7095545f76ce70d4
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-12-11 14:51:43 -0300

    OK

---
+++ js/exp-products.js
@@ -0,0 +1,25 @@
+
+/*-----------------EXP-PRODUCTOS----------------- */
+
+export const productosMujer = [
+    {
+        id: 1,
+        nombre: "Remera Modal Soft",
+        precio: 7500,
+        categoria: "remeras",
+        imagen: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR 1.jpeg"],
+        etiqueta: "Novedad",
+        variantes: [{ color: "celeste", talla: "S", stock: 3 }]
+    },
+    {
+        id: 2,
+        nombre: "Calza Nike Pro",
+        precio: 13500,
+        categoria: "calzas",
+        imagen: ["img/mujer/calzas/front.jpeg"],
+        etiqueta: "Novedad",
+        variantes: [{ color: "negro", talla: "S", stock: 2 }]
+    },
+    // Otros productos...
+];
+
