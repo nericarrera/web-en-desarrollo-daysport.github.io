35c3d27a0653e374a2993cb12969a1cf9e3d86ff
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-12-12 10:44:09 -0300

    ok

--- js/exp-products.js
+++ js/exp-products.js
@@ -3,20 +3,15 @@
 
 export const productosMujer = [
     {
-        id: 1,
-        nombre: "Remera Modal Soft",
-        precio: 7500,
-        categoria: "remeras",
-        imagen: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg", "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg"],
-        miniaturas: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg", "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg"],
-        hoverImagenes: ["img/mujer/remera-modal-soft-cuelloR/hover1.jpeg"],
-        etiqueta: "Novedades",
-        variantes: [
-            { color: "celeste", talla: "S", stock: 3 },
-            { color: "rojo", talla: "M", stock: 0 }, // Sin stock
-            { color: "azul", talla: "L", stock: 5 }
-        ]
+      id: 1,
+      nombre: "Remera Modal Soft",
+      precio: 7500,
+      categoria: "remeras",
+      etiqueta: "novedad",
+      imagen: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg"],
     },
+    // Otros productos
+  ];
  
     {
         id: 2,
