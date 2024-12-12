26aad38de6737bf201585753726a7af36a55521d
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2024-12-12 10:27:04 -0300

    ok

--- js/exp-products.js
+++ js/exp-products.js
@@ -7,13 +7,17 @@ export const productosMujer = [
         nombre: "Remera Modal Soft",
         precio: 7500,
         categoria: "remeras",
-        imagen: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg"],
-        miniaturas: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg"],
-        etiqueta: "novedad",
+        imagen: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg", "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg"],
+        miniaturas: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg", "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg"],
+        hoverImagenes: ["img/mujer/remera-modal-soft-cuelloR/hover1.jpeg"],
+        etiqueta: "Novedades",
         variantes: [
             { color: "celeste", talla: "S", stock: 3 },
-        ],
+            { color: "rojo", talla: "M", stock: 0 }, // Sin stock
+            { color: "azul", talla: "L", stock: 5 }
+        ]
     },
+ 
     {
         id: 2,
         nombre: "Calza Nike Pro",
