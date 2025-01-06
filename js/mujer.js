901e0ce736668c9a4b9645406004f8c9a38fa41e
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-01-06 11:32:26 -0300

    ok

--- js/mujer.js
+++ js/mujer.js
@@ -38,6 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     
                 ],
                 negro: [
+                    "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg",
                     "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg"
                    
                 ],
