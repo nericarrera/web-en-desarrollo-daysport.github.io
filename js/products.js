e22c159f93c7c55a2bec788d5df514244f8eba15
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-01-22 17:55:16 -0300

    ok

--- js/products.js
+++ js/products.js
@@ -55,12 +55,9 @@
   
           coloresContainer.appendChild(colorThumbnail);
       });
+    });
+      
   
-      // Mostrar talles del primer color por defecto
-      const colorInicial = coloresUnicos[0];
-      actualizarGaleria(product, colorInicial);
-      actualizarTalles(product, colorInicial);
-  });
   
   // Actualizar galería según el color seleccionado
   function actualizarGaleria(product, color) {
