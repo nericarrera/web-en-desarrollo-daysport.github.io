ba597b9f50f61ef20eaffcfab1bd774a4244014e
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-02-18 15:17:57 -0300

    ok

--- js/mujerProductos.js
+++ js/mujerProductos.js
@@ -2,7 +2,7 @@
 export  const productosMujer = [
         
     {
-        id: "mujer-1",
+    id: "mujer-1",
     nombre: "Remera Modal Soft",
     precio: 8000,
     categoria: "remeras",
@@ -14,6 +14,7 @@ export  const productosMujer = [
         { src: "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg" },
         { src: "img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-2.jpeg" }
     ],
+    hoverImagenes: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg"], // Hover en el carrusel
     variantes: [
         { color: "celeste", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
         { color: "negro", talla: "L", stock: 2, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
@@ -31,6 +32,7 @@ export  const productosMujer = [
     {
         id: "mujer-2",
         nombre: "Calza Nike Pro",
+        etiqueta: "novedad",
         precio: 14000,
         categoria: "calzas",
         seccion: "mujer",
@@ -41,8 +43,7 @@ export  const productosMujer = [
            { src: "img/mujer/calzas/calza-nike-pro-gris-1.jpeg" }, 
            { src: "img/mujer/calzas/calza-nike-pro-neg-1.jpeg" },
         ],
-        hoverImagenes: ["img/mujer/calzas/calza-nike-radeon-1.jpeg"],
-        etiqueta: "novedad",
+        hoverImagenes: ["img/mujer/calzas/calza-nike-radeon-1.jpeg"], // Hover en el carrusel
         variantes: [
             { color: "negro", talla: "L", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "gris", talla: "L", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
@@ -59,18 +60,19 @@ export  const productosMujer = [
 
     { id: "mujer-3",
         nombre: "Campera Deportiva Nike",
-        precio: 13500,
+        etiqueta: "novedad",
+        precio: 30000,
         categoria: "camperas",
         seccion: "mujer",
         temporada: "media estacion",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
         imagen: ["img/mujer/camperas-deportivas/campera-deportiva-nike-1.jpeg", "img/mujer/camperas-deportivas/campera-deportiva-nike-2.jpeg", "img/mujer/camperas-deportivas/campera-deportiva-nike-3.jpeg"], // Imagen principal para el carrusel
-        hoverImagenes: ["img/mujer/camperas-deportivas/campera-deportiva-nike-5.jpeg"], // Hover en el carrusel
         miniaturas: [
-            "img/mujer/camperas-deportivas/campera-deportiva-nike-1.jpeg",
-            "img/mujer/camperas-deportivas/campera-deportiva-nike-2.jpeg",
-            "img/mujer/camperas-deportivas/campera-deportiva-nike-3.jpeg"
+            { src: "img/mujer/camperas-deportivas/campera-deportiva-nike-1.jpeg" },
+            { src: "img/mujer/camperas-deportivas/campera-deportiva-nike-2.jpeg" },
+            { src: "img/mujer/camperas-deportivas/campera-deportiva-nike-3.jpeg" }
         ], // Miniaturas para el carrusel
-        etiqueta: "novedad",
+        hoverImagenes: ["img/mujer/camperas-deportivas/campera-deportiva-nike-5.jpeg"], // Hover en el carrusel
         variantes: [
             { color: "negro", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "negro", talla: "L", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
@@ -92,14 +94,18 @@ export  const productosMujer = [
         
     { id: "mujer-4", 
         nombre: "Blusa de Lino", 
+        etiqueta: "novedad",
         precio: 9000, 
         categoria: "remeras",
         seccion: "mujer",
         temporada: "verano", 
-        imagen: ["img/mujer/remeras-lino/blusalino-negro-1.jpeg"], 
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
+        imagen: ["img/mujer/remeras-lino/blusalino-negro-1.jpeg"],
+        miniaturas: [ 
+            { src: "img/mujer/remeras-lino/blusalino-negro-1.jpeg" }
+
+        ], 
         hoverImagenes: [],
-        miniaturas: ["img/mujer/remeras-lino/blusalino-negro-1.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "negro", talla: "XL", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
         ],
@@ -113,19 +119,23 @@ export  const productosMujer = [
     },
 
     { id: "mujer-5", 
-        nombre: "Calza Nike Radeon", 
+        nombre: "Calza Nike Radeon",
+        etiqueta: "novedad", 
         precio: 13500, 
         categoria: "calzas", 
         seccion: "mujer",
         temporada: "verano",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
         imagen: ["img/mujer/calzas/calza-nike-radeon-1.jpeg"], 
+        miniaturas: [ 
+            { src: "img/mujer/calzas/calza-nike-radeon-1.jpeg" }
+
+        ],
         hoverImagenes: [],
-        miniaturas: ["img/mujer/calzas/calza-nike-radeon-1.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "negro", talla: "S", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "gris", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
-        ],
+        ],       
         imagenColores: { // Imágenes específicas por color para la página de producto
             negro: [
                 "img/mujer/calzas/calza-nike-radeon-1.jpeg",
@@ -138,19 +148,23 @@ export  const productosMujer = [
     },
 
     { id: "mujer-6", 
-        nombre: "Calza Nike Grofada", 
+        nombre: "Calza Nike Grofada",
+        etiqueta: "novedad", 
         precio: 15000, 
         categoria: "calzas", 
         seccion: "mujer",
         temporada: "verano",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
         imagen: ["img/mujer/calzas/calza-nike-grofada-1.jpeg"], 
+        miniaturas: [ 
+            { src: "img/mujer/calzas/calza-nike-grofada-1.jpeg" }
+
+        ],
         hoverImagenes: [],
-        miniaturas: ["img/mujer/calzas/calza-nike-grofada-1.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "negro", talla: "S", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "gris", talla: "M", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
-        ],
+        ],        
         imagenColores: { // Imágenes específicas por color para la página de producto
             negro: [
                 "img/mujer/calzas/calza-nike-grofada-1.jpeg",
@@ -162,20 +176,26 @@ export  const productosMujer = [
             ],
         }
     },
-    { id: "mujer-7", 
+
+    {   
+        id: "mujer-7", 
         nombre: "Calza Nike Speak", 
+        etiqueta: "novedad",
         precio: 13500, 
         categoria: "calzas", 
         seccion: "mujer",
         temporada: "verano",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
         imagen: ["img/mujer/calzas/calza-nike-speak-1.jpeg"], 
+        miniaturas: [ 
+            { src: "img/mujer/calzas/calza-nike-speak-1.jpeg" }
+
+        ],
         hoverImagenes: [],
-        miniaturas: ["img/mujer/calzas/calza-nike-speak-1.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "negro", talla: "S", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "gris", talla: "M", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
-        ],
+        ],        
         imagenColores: { // Imágenes específicas por color para la página de producto
             negro: [
                 "img/mujer/calzas/calza-nike-speak-1.jpeg", 
@@ -188,14 +208,18 @@ export  const productosMujer = [
     },
     { id: "mujer-8", 
         nombre: "Calza Nike Fluorecent", 
+        etiqueta: "novedad",
         precio: 13500, 
         categoria: "calzas",
         seccion: "mujer",
-        temporada: "invierno", 
+        temporada: "invierno",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.", 
         imagen: ["img/mujer/calzas/calza-nike-fluor-1.jpeg", "img/mujer/calzas/calza-nike-fluor-2.jpeg"], 
+        miniaturas: [ 
+            { src: "img/mujer/calzas/calza-nike-fluor-2.jpeg" }
+
+        ],
         hoverImagenes: [],
-        miniaturas: ["img/mujer/calzas/calza-nike-fluor-2.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "rosa", talla: "L", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "rosa", talla: "XL", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
@@ -207,20 +231,24 @@ export  const productosMujer = [
           
         } 
     },
+
     { id: "mujer-9", 
         nombre: "Calza Adidas Original", 
+        etiqueta: "novedad",
         precio: 13500, 
         categoria: "calzas", 
         seccion: "mujer",
         temporada: "verano",
+        descripcion: "Calza deportiva excelente calidad, ideal para realizar deporte de alto rendimiento.",
         imagen: ["img/mujer/calzas/calza-adidas-original-1.jpeg"], 
+        miniaturas: [ 
+            { src: "img/mujer/calzas/calza-adidas-original-1.jpeg" }
+        ],
         hoverImagenes: [],
-        miniaturas: ["img/mujer/calzas/calza-adidas-original-1.jpeg"],
-        etiqueta: "novedad",
         variantes: [
             { color: "negro", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
             { color: "multicolor", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" }
-        ],
+        ],        
         imagenColores: { // Imágenes específicas por color para la página de producto
             negro: [
                 "img/mujer/camperas-deportivas/campera-deportiva-nike-negro-frente.jpeg",
