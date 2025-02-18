5ddd993800d3121ad81fcd825dcd80b0f3c822da
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-02-18 17:13:05 -0300

    ok

--- js/mujer.js
+++ js/mujer.js
@@ -12,7 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Productos disponibles:", productosMujer);
     const mujerProductsGrid = document.querySelector('.mujer-products-grid');
 
-    
+    if (!mujerProductsGrid) {
+        console.error("No se encontró el contenedor de productos.");
+        return;
+    }
+
     // Mostrar productos
     productosMujer.forEach(producto => {
         console.log("Renderizando producto:", producto.nombre);
