e483a105631c0330b569e85c62f9060dc4be2cfc
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-02-22 16:11:46 -0300

    OK

--- js/mujerProductos.js
+++ js/mujerProductos.js
@@ -17,8 +17,18 @@ export  const productosMujer = [
     ],
     hoverImagenes: ["img/mujer/remera-modal-soft/remera-modal-soft-cuelloR-1.jpeg"], // Hover en el carrusel
     variantes: [
+        { color: "negro", talla: "S", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106"},
+        { color: "negro", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106"},
+        { color: "negro", talla: "XL", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106"},
+        { color: "negro", talla: "XXL", stock: 0, pecho: "100-104", cintura: "86-90", cadera: "102-106"},
+        { color: "celeste", talla: "S", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
         { color: "celeste", talla: "M", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
-        { color: "negro", talla: "L", stock: 2, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
+        { color: "celeste", talla: "L", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
+        { color: "celeste", talla: "XL", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
+        { color: "celeste", talla: "XXL", stock: 1, pecho: "100-104", cintura: "86-90", cadera: "102-106" },
+        
+        
+        
     ],
     imagenColores: {
         celeste: [
