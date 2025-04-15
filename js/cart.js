f7ca05655e41aaba7cf217b47ced19ead7f8be3e
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-04-14 23:16:29 -0300

    ok

--- js/cart.js
+++ js/cart.js
@@ -131,15 +131,6 @@ document.addEventListener('DOMContentLoaded', function() {
     updateCart();
   });
 
-  // Verifica si el carrito está inicializado
-console.log('Carrito:', JSON.parse(localStorage.getItem('cart')));
-
-// Verifica visibilidad del dropdown
-console.log('Estilo del dropdown:', document.getElementById('cart-dropdown').style.display);
-
-// Fuerza mostrar el carrito
-document.getElementById('cart-dropdown').classList.remove('cart-dropdown-hidden');
-
 
 document.getElementById('cart-icon').addEventListener('click', function() {
     alert('Icono clickeado!');
