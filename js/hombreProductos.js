7572652c92ba39f98bba40fde72e31229e88cc5f
NeriCarrera <103610758+nericarrera@users.noreply.github.com>
2025-04-18 00:55:20 -0300

    ok

--- js/hombreProductos.js
+++ js/hombreProductos.js
@@ -12,6 +12,7 @@ export const productosHombre = [
         descripcion: "Buzo de algodon frizado, cuello redondo liso, para usar en cualquier ocasion",
         imagen: ["img/hombre/buzos/buzo-lacoste-azulfran-1.jpeg"],
         miniaturas: [
+            {src: "img/hombre/buzos/buzo-lacoste-azulfran-1.jpeg"},
 
         ],
         hoverImagenes: ["img/hombre/buzos/buzo-lacoste-azulfran-1.jpeg"], // Hover en el carrusel
